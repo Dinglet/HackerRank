4377bc7d92fc1eaa0118574a56cae85452c59565
Ding Huang <jeff0815jeff0815@hotmail.com>
2018-01-28 20:48:18 +0800

    Create MakingAnagrams.cs

---
+++ Tutorials/Cracking the Coding Interview/MakingAnagrams.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace MakingAnagrams
+{
+    class Program
+    {
+        class Bucket
+        {
+            private Hashtable keyCountPairs = new Hashtable();
+            public char[] Keys{get{return keyCountPairs.Keys.Cast<char>().ToArray();}}
+            public int this[char key]
+            {
+                get{return Convert.ToInt32(keyCountPairs[key]);}
+                set{keyCountPairs[key] = value;}
+            }
+            public bool Contains(char key)
+            {
+                return keyCountPairs.ContainsKey(key);
+            }
+            public Bucket(string str)
+            {
+                var query = 
+                    from letter in str
+                    group letter by letter;
+                foreach(IGrouping<char, char> charGroup in query)
+                {
+                    // Console.WriteLine(charGroup.Key + " " + charGroup.Count());
+                    keyCountPairs.Add(charGroup.Key, charGroup.Count());
+                }
+            }
+            public static int CountDifference(Bucket a, Bucket b)
+            {
+                int count = 0;
+                var keys = a.Keys.Union(b.Keys);
+                foreach(var key in keys)
+                {
+                    if(a.Contains(key))
+                        if(b.Contains(key))
+                            count += Math.Abs(a[key]-b[key]);
+                        else
+                            count += a[key];
+                    else
+                        count += b[key];
+                }
+                return count;
+            }
+        }
+        static void Main(string[] args) {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            Bucket bucketA = new Bucket(a), bucketB = new Bucket(b);
+            int numOfDifference = Bucket.CountDifference(bucketA, bucketB);
+            Console.WriteLine(numOfDifference);
+        }
+    }
+}
